fee01eec592e97a61fe385212f57662c3cb5cbad
BananaWafflez <benaing12@gmail.com>
2023-07-24 19:52:10 -0400

    Completed rock paper scissors function

--- external.js
+++ external.js
@@ -1,4 +1,42 @@
+function getComputerChoice(){
+    return Math.floor(Math.random() * 3);
+    
+}
+
 function playRound(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "It's a tie!";
+    }
+    else if(playerSelection == 0){
+        if(computerSelection == 1){
+            return "Computer won! It picked paper!";
+        }
+        else{
+            return "You Win!";
+        }
+    }
+    else if(playerSelection == 1){
+        if(computerSelection == 0){
+            return "You Win!";
+        }
+        else{
+            return "Computer won! It picked scissors!";
+        }
+    } 
+    else{
+        if(computerSelection == 0){
+            return "Computer won! It picked rock!"
+        }
+        else{
+            return "You Win!";
+        }
+    }
 
 }
-console.log("Test")
+//console.log("Choose either rock, paper, or scissors")
+const playerSelection = prompt("0. Rock, 1. Paper, 2. Scissors");
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+
+console.log(playRound(playerSelection, computerSelection));
+
