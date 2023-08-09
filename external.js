c6f8d8f7a61a0817929936879d49109e20841adc
BananaWafflez <benaing12@gmail.com>
2023-08-09 13:19:29 -0400

    Added functioning scoreboard

--- external.js
+++ external.js
@@ -2,32 +2,50 @@ function getComputerChoice(){
     return Math.floor(Math.random() * 3);
     
 }
-
+function updateScore(){
+    const player = document.getElementById("playerscore");
+    const computer = document.getElementById("computerscore")
+    console.log(player);
+    player.textContent = `Player: ${playerscore}`;
+    computer.textContent = `Computer: ${computerscore}`;
+}
 function playRound(playerSelection, computerSelection){
     if(playerSelection == computerSelection){
         return "It's a tie!";
     }
     else if(playerSelection == 0){
         if(computerSelection == 1){
+            computerscore++;
+            updateScore()
             return "Computer won! It picked paper!";
         }
         else{
+            playerscore++;
+            updateScore()
             return "You Win!";
         }
     }
     else if(playerSelection == 1){
         if(computerSelection == 0){
+            playerscore++;
+            updateScore()
             return "You Win!";
         }
         else{
+            computerscore++;
+            updateScore()
             return "Computer won! It picked scissors!";
         }
     } 
     else{
         if(computerSelection == 0){
+            computerscore++;
+            updateScore()
             return "Computer won! It picked rock!"
         }
         else{
+            playerscore++
+            updateScore()
             return "You Win!";
         }
     }
@@ -43,19 +61,19 @@ buttons.forEach((button) => {
     button.addEventListener('click', function(e){
         if(e.target.className == "rock"){
             playerSelection = 0;
-            console.log(playerSelection);
+            
             const computerSelection = getComputerChoice();
             console.log(playRound(playerSelection, computerSelection));
         }
         else if(e.target.className == "paper"){
             playerSelection = 1;
-            console.log(playerSelection);
+            
             const computerSelection = getComputerChoice();
             console.log(playRound(playerSelection, computerSelection));
         }
         else{
             playerSelection = 2; 
-            console.log(playerSelection);
+            
             const computerSelection = getComputerChoice();
             console.log(playRound(playerSelection, computerSelection));
         }
@@ -63,6 +81,7 @@ buttons.forEach((button) => {
     });
     
 });
+
 
 //const computerSelection = getComputerChoice();
 //console.log(computerSelection);
